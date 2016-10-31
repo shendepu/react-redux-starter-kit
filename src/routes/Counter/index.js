b9eecb538453dc726a9ca4555b3b1ceecfe1bb85
SHEN, JIMMY <shendepu@hotmail.com>
2016-10-31 16:18:23 +0800

    Changed to require.ensure

--- src/routes/Counter/index.js
+++ src/routes/Counter/index.js
@@ -5,15 +5,33 @@ export default (store) => ({
   pattern : 'counter',
   /*  Async getComponent is only invoked when route matches   */
   component: asyncComponent(() => {
-    return Promise.all([
-        System.import('./containers/CounterContainer'),
-        System.import('./modules/counter')])
-      .then(([Counter, reducer]) => {
-        injectReducer(store, { key: 'counter', reducer: reducer.default })
-        console.log('------- Counter ')
-        console.log(Counter.default)
-        return Counter
-      })
-    }
+    return new Promise(resolve => {
+      require.ensure([], (require) => {
+        /*  Webpack - use require callback to define
+         dependencies for bundling   */
+        const Counter = require('./containers/CounterContainer')
+        const reducer = require('./modules/counter').default
+
+        /*  Add the reducer to the store on key 'counter'  */
+        injectReducer(store, {key: 'counter', reducer})
+
+        /*  Return getComponent   */
+        resolve(Counter)
+        /* Webpack named bundle   */
+      }, 'counter')
+
+    })
+  }
+//
+//     return Promise.all([
+//         System.import('./containers/CounterContainer'),
+//         System.import('./modules/counter')])
+//       .then(([Counter, reducer]) => {
+//         injectReducer(store, { key: 'counter', reducer: reducer.default })
+//         console.log('------- Counter ')
+//         console.log(Counter.default)
+//         return Counter
+//       })
+//     }
   )
 })
--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -1,9 +1,9 @@
-// import HomeView from './components/HomeView'
-import asyncComponent from '../../components/asyncComponent'
+import HomeView from './components/HomeView'
+// import asyncComponent from '../../components/asyncComponent'
 
 // Sync route definition
 export default {
   pattern: '/',
   exactly: true,
-  component : asyncComponent(() => System.import('./components/HomeView'))
+  component : HomeView
 }
