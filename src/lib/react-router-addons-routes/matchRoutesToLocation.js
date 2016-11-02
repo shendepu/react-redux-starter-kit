eeae7fe124a0f8326a99f4b12c5c4ee6ae35fbd7
SHEN, JIMMY <shendepu@hotmail.com>
2016-11-02 20:52:48 +0800

    Added exactly in matchRoutesToLocation

--- src/lib/react-router-addons-routes/matchRoutesToLocation.js
+++ src/lib/react-router-addons-routes/matchRoutesToLocation.js
@@ -1,10 +1,10 @@
 import matchPattern from 'react-router/matchPattern'
 import { mergePatterns } from './util'
 
-const matchRoutesToLocation = (routes, location, matchedRoutes = [], params = {}, parentPattern = '') => {
+const matchRoutesToLocation = (routes, location, matchedRoutes = [], params = {}, parentPattern = '/') => {
   routes.forEach((route) => {
     const nestedPattern = mergePatterns(parentPattern, route.pattern)
-    const match = matchPattern(nestedPattern, location)
+    const match = matchPattern(nestedPattern, location, route.exactly)
 
     if (match) {
       matchedRoutes.push(route)
